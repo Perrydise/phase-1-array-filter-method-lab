291f065aa92821dac3b4bfb84cc06f0bf724ed42
Perry Jorss <pjorss123@gmail.com>
2022-04-20 22:00:10 -0400

    commit tree c63bf5d3b6829854c7cf5e76d69c1604c770ca74

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+function findMatching(driversArray, itemToFind){
+    const result = driversArray.filter(element => element.toLowerCase() === itemToFind.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(driversArray, itemFinder){
+    const result = driversArray.filter(element => element.startsWith(itemFinder))
+    return result
+}
+
+function matchName(driverArray, findThis){
+    const result = driverArray.filter(element => {
+        console.log(element, element.name, element.name.includes(findThis))
+        return element.name.includes(findThis)
+    })
+    return result
+}
